bba087921c54984eb3399516f2aeb1d4d6e56fe8
Ezio Lin <ninesuns.lin@outlook.com>
2024-04-17 21:47:56 +0800

    ✅ skip graph test
    图查询还需要懂得引入更多上下文，先把简单的功能实现了多熟悉一下 LangChain 再回头来做

--- test/graph.test.ts
+++ test/graph.test.ts
@@ -1,7 +1,7 @@
 import { expect, test, describe } from 'vitest'
 import { trpcCaller } from './helper.js';
 
-describe("a natural language interface to Cypher query language", () => {
+describe("a natural language interface to Cypher query language", { skip: true }, () => {
 
     test("驱动正常可用连接数据", () => {
         return trpcCaller.graph.test().then((res) => {
