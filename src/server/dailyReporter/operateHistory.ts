c0a7f669677417ad2174d8d341a407f633a00efa
Ezio Lin <ninesuns.lin@outlook.com>
2024-04-20 00:26:21 +0800

    🚧 操作日期的搜索范围变更
    从当天 4 点反推到前一天 0 点，照顾一些卷王的记录

--- src/server/dailyReporter/operateHistory.ts
+++ src/server/dailyReporter/operateHistory.ts
@@ -1,15 +1,25 @@
 import * as Id from 'fp-ts/Identity';
 import { pipe } from 'fp-ts/function';
 import { subDays, setHours, isWithinInterval } from 'date-fns/fp';
-import { ZhiweiClient, operateHistory } from '../../zhiwei/index.js'
+import { ZhiweiClient, operateHistory } from '../../zhiwei/index.js';
 
 export const getSomeoneOperateHistoryWithinYesterday = (client: ZhiweiClient, userIdentity: string) => {
     const api = operateHistory(client);
-    // `operateHistory` 是滚动加载分页的，目前没有针对人的过滤 所以只有连续调用 
 
-    const searchRange = pipe(new Date(), Id.bindTo("end"), Id.bind("start", ({ end }) => pipe(end, subDays(1))));
+    // 搜索范围 从当天 4 点反推到前一天 0 点
+    const searchRange = pipe(
+        new Date(),
+        setHours(4),
+        Id.bindTo('end'),
+        Id.bind('start', ({ end }) => pipe(end, subDays(1), setHours(0)))
+    );
 
-    const searchRec = (acc: Awaited<ReturnType<ReturnType<typeof operateHistory>>>, curPage: number): Promise<typeof acc> => {
+    // `operateHistory` 是滚动加载分页的，目前没有针对人的过滤 所以只有连续调用
+
+    const searchRec = (
+        acc: Awaited<ReturnType<ReturnType<typeof operateHistory>>>,
+        curPage: number
+    ): Promise<typeof acc> => {
         return api(curPage, 'card').then((res) => {
             const result = res.filter((it) => pipe(new Date(it.createdAt), isWithinInterval(searchRange)));
             if (result.length === 0) {
@@ -19,7 +29,7 @@ export const getSomeoneOperateHistoryWithinYesterday = (client: ZhiweiClient, us
                 return searchRec(acc, curPage + 1);
             }
         });
-    }
+    };
 
-    return searchRec([], 0).then(it => it.filter(it => it.resource.memberName === userIdentity));
-}
+    return searchRec([], 0).then((it) => it.filter((it) => it.resource.memberName === userIdentity));
+};
--- test/daily-report.test.ts
+++ test/daily-report.test.ts
@@ -3,8 +3,7 @@ import { trpcCaller } from './helper.js';
 
 describe('日报助手', () => {
     test('使用 zhiwei api 获取历史', () => {
-        return trpcCaller.dailyReporter.gen({ email: 'linjx@agilean.cn' }).then((it) => {
-            console.log(it);
+        return trpcCaller.dailyReporter.gen({ email: 'chenmy@agilean.cn' }).then((it) => {
             expect(it).toBeInstanceOf(Array);
             expect(it.length).toBeGreaterThan(0)
         });
