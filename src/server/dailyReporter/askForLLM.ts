402aa61774190f3dbb3a788e8ec8d293e28d2459
Ezio Lin <ninesuns.lin@outlook.com>
2024-04-24 00:10:18 +0800

    🚧 调整一下日报生成的 prompt
    之前的 system 好像导致 LLM 输出太过僵硬了 重新从零开始加

--- src/server/dailyReporter/askForLLM.ts
+++ src/server/dailyReporter/askForLLM.ts
@@ -12,18 +12,20 @@ JSONSchema:
 {schema}
 \`\`\`
 
-操作内容如果是：\`从「A」到「B」\`的话，代表了精益敏捷中卡片在价值流的移动，以下是几个常见的移动场景用 \`内容 -> 含义\` 来表示
-- 从「Story development-Complete story」移动到「Be deployed」-> 故事完成开始进入测试
-- 从「acceptance-testing」移动到「acceptance-Acceptance」 -> 故事测试完成等待验收
-- 从「Solve verification」移动到「TKB verification」-> 缺陷验证完成
+操作内容如果是：\`从「A」到「B」\`的话，代表了精益敏捷中卡片在价值流的移动，以下是几个常见的 「A」和 「B」所属的价值流
+- 研发用户故事看板：「Story development-Complete story」、「Be deployed」、「Story development-Story research and development」、「Story development-Complete story」
+- 代码评审看板：「To be reviewed」、「Appraise」、「To be discussed」
+- 研发缺陷看板：「Discover」、「priority」、「Repair」、「Solve verification」
+
+以下是价值流看板的应用场景：
+- 研发用户故事看板：跟踪管理研发过程中的用户故事价值流单元，用户故事的右移体现了业务价值的逐渐产生。用来跟踪记录产品的增值过程
+- 代码评审看板：对代码提交任务进行评审，对代码进行讨论评估，对有缺陷风险的移入讨论，对无问题的代码移动通过。用来对代码质量进行提升守护
+- 研发缺陷看板：跟踪处理研发过程中产生的缺陷问题，及时修复，及时验证。用于提升产品质量
 
 以下是对你的要求：
-- 需要尊重\`工作记录\`的事实基础，做到每个概括都能从\`工作记录\`中找到依据。
-- 需要以第一人称的角度思考，把你当成主人翁，但是要是总结中不要出现 "我" 这类人称代词，也不要出现 "可能"，"也许" 这类词语。
-- 如果操作的卡片名称符合 "#数字-人名" 的话，请忽略整条内容。
-- 尝试推测出这些内容背后的行为目的，不用出现原始的操作的内容。记住，如果推测不出来就不要推测。
-- 如果\`工作记录\`的操作卡名称是一个人名的话，省略掉它。
-- 可用适当合并一下可能相关的卡片到同一个列表项中，例如，都是在处理同一类任务
+- 将 从「A」到「B」这类的价值单元移动卡片，概括背后的含义，不要出现 从「A」到「B」
+- 将所有的代码评审相关的\`工作记录\`概括成：处理了代码评审任务
+- 只合并所有的：“处理了代码评审任务” 成一条
 
 {format_instructions}
 `;
@@ -37,7 +39,7 @@ const cardOperateHistoryPromptTpl = `\
 
 
 const schemaCardOperateRecord = Z.record(
-    Z.string().describe("操作的卡片名称"),
+    Z.string().describe("操作的卡片名称，由卡片编号和卡片名称组成"),
     Z.array(Z.string().describe("操作的内容")).describe("一张卡片所有的所有操作内容")
 ).describe("工作记录");
 
@@ -47,7 +49,7 @@ export type CardOperateRecord = Z.infer<typeof schemaCardOperateRecord>;
 const jsonSchema = JSON.stringify(zodToJsonSchema(schemaCardOperateRecord, '工作记录'))
 
 export const askForLLM = async (cardOperateHistory: CardOperateRecord) => {
-    const model = new ChatOpenAI({ temperature: 0.3, modelName: 'gpt-3.5-turbo-0125' });
+    const model = new ChatOpenAI({ temperature: 0.8, modelName: 'gpt-3.5-turbo-1106' });
 
     const prompt = ChatPromptTemplate.fromMessages<Record<'schema' | 'history' | 'format_instructions', string>>([
         ['system', systemPromptTpl],
@@ -55,7 +57,7 @@ export const askForLLM = async (cardOperateHistory: CardOperateRecord) => {
     ]);
 
     const outputParser = StructuredOutputParser.fromZodSchema(
-        Z.array(Z.string().describe('工作内容')).describe('工作项列表')
+        Z.array(Z.string().describe('一般为体现 #+数字卡片编号开头的工作内容描述，例如 "#94409-前端配置项迁移: 完成了桌面检查并移交了测试"')).describe('工作项列表')
     );
 
     const chain = prompt.pipe(model).pipe(outputParser);
