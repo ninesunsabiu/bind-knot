a6b49a570d2a815368d3e3ae3d29a065103d29b6
Ezio Lin <ninesuns.lin@outlook.com>
2024-04-30 23:58:27 +0800

    🐛 操作历史在日期范围内获取不正确的问题
    在模拟滚动翻页获取更多的时候，不能使用 inInterval，否则当第一页全部晚于 end 日期时，所有项都不满足
    然后无法翻页，也就无法获得正确的日期范围

--- src/server/dailyReporter/operateHistory.ts
+++ src/server/dailyReporter/operateHistory.ts
@@ -1,5 +1,5 @@
 import { pipe } from 'fp-ts/function';
-import { isWithinInterval } from 'date-fns/fp';
+import { isAfter, isBefore } from 'date-fns/fp';
 import { type ZhiweiClient, operateHistory } from '~/zhiwei/index.js';
 import { prevWorkRange } from './chinaHoliday.js';
 
@@ -7,27 +7,30 @@ export const getSomeoneOperateHistoryWithinYesterday = (client: ZhiweiClient, us
     const api = operateHistory(client);
 
     // 搜索范围 从当天 4 点反推到前一天 0 点
-    const searchRange = prevWorkRange(new Date())
+    const searchRange = prevWorkRange(new Date());
 
     // `operateHistory` 是滚动加载分页的，目前没有针对人的过滤 所以只有连续调用
 
     const searchRec = (
-        searchRange: { start: Date, end: Date },
+        start: Date,
         acc: Awaited<ReturnType<ReturnType<typeof operateHistory>>>,
         curPage: number
     ): Promise<typeof acc> => {
         return api({ page: curPage, keyword: userIdentity }, 'card').then((res) => {
-            const result = res.filter((it) => pipe(new Date(it.createdAt), isWithinInterval(searchRange)));
+            const result = res.filter((it) => pipe(it.createdAt, isAfter(start)));
             if (result.length === 0) {
                 return acc;
             } else {
                 acc.push(...result);
-                return searchRec(searchRange, acc, curPage + 1);
+                return searchRec(start, acc, curPage + 1);
             }
         });
     };
 
-    return searchRange.then((s) =>
-        searchRec(s, [], 0).then((it) => it.filter((it) => it.resource.memberName === userIdentity))
-    );
+    return searchRange.then((s) => {
+        const end = s.end;
+        return searchRec(s.start, [], 0)
+            .then((it) => it.filter((it) => pipe(it.createdAt, isBefore(end))))
+            .then((it) => it.filter((it) => it.resource.memberName === userIdentity));
+    });
 };
