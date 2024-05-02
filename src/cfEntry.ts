8c8d81afd03cdbf9de3fcf0917ebb628726ce143
Ezio Lin <ninesuns.lin@outlook.com>
2024-05-03 00:10:52 +0800

    ✨ cf request add CORS config

--- src/cfEntry.ts
+++ src/cfEntry.ts
@@ -5,20 +5,28 @@ import { ofClient } from './zhiwei/client.js';
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
         return fetchRequestHandler({
+            responseMeta: () => {
+                return {
+                    headers: {
+                        'Access-Control-Allow-Origin': 'https://tkb.agilean.cn',
+                        'Access-Control-Allow-Methods': 'GET,POST'
+                    }
+                };
+            },
             endpoint: '/api',
             req: request,
             router: appRouter,
             createContext: async () => {
-				return {
-					zhiweiClient: await ofClient({
-						orgIdentity: env.ZHIWEI_ORG_IDENTITY,
-						password: env.ZHIWEI_PASSWORD,
-						username: env.ZHIWEI_USERNAME,
-						baseUrl: env.ZHIWEI_BASE_URL
-					}),
-					env
-				}
-			}
+                return {
+                    zhiweiClient: await ofClient({
+                        orgIdentity: env.ZHIWEI_ORG_IDENTITY,
+                        password: env.ZHIWEI_PASSWORD,
+                        username: env.ZHIWEI_USERNAME,
+                        baseUrl: env.ZHIWEI_BASE_URL
+                    }),
+                    env
+                };
+            }
         });
     }
 };
